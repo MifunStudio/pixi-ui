9628a78e5eeb2fcba48676c976a66d8e0a359aca
Zhang张政明 <zhang.zhengming@immomo.com>
2016-02-17 16:26:02 +0800

    updated

--- index.js
+++ index.js
@@ -788,7 +788,7 @@ module.exports =
 	                var child = children[i];
 	                start += this.childMargin.top;
 	                child.position.y = start;
-	                start += UI.getHeight(child) + this.childMargin.right;
+	                start += UI.getHeight(child) + this.childMargin.bottom;
 	                size.width = Math.max(UI.getWidth(child), size.width);
 	            }
 	            size.height = start;
